20c56dccdf6ee5f777e6c754f82e176266893d49
Pablo <ardevol8@gmail.com>
2016-05-05 16:52:48 +0200

    Test solution added and service modified.

---
+++ SynUp_Desktop/SynUp_DesktopTests/model/dao/ConnectionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SynUp_Desktop.model.dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynUp_Desktop.model.dao.Tests
+{
+    [TestClass()]
+    public class ConnectionTests
+    {
+        [TestMethod()]
+        public void createTaskTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void readTaskTest()
+        {
+            Assert.Equals(null , Connection.readTask("something"));
+        }
+    }
+}
---
+++ SynUp_Desktop/SynUp_DesktopTests/service/ServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SynUp_Desktop.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynUp_Desktop.service.Tests
+{
+    [TestClass()]
+    public class ServiceTests
+    {
+        [TestMethod()]
+        public void createTaskTest()
+        {
+            /*
+            bool resultat = false;
+
+            Service servei = new Service();
+
+            //String code, String name, DateTime priorityDate, String description, String localization, String project
+            resultat = servei.createTask("1","Projecte",new DateTime(),null,null,null);
+
+            //Assert.AreEqual(true, resultat);
+            */
+            
+        }
+    }
+}
